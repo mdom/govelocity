5369bf5395e49808f0ec3e4f7b81c8658d40f368
Mario Domgoergen <mario@domgoergen.com>
2021-05-10 23:26:40 +0200

    Use default colors

--- main.go
+++ main.go
@@ -12,6 +12,20 @@ import (
     "time"
 )
 
+var theme = tview.Theme{
+    PrimitiveBackgroundColor:    tcell.ColorDefault,
+    ContrastBackgroundColor:     tcell.ColorDefault,
+    MoreContrastBackgroundColor: tcell.ColorDefault,
+    BorderColor:                 tcell.ColorDefault,
+    TitleColor:                  tcell.ColorDefault,
+    GraphicsColor:               tcell.ColorDefault,
+    PrimaryTextColor:            tcell.ColorDefault,
+    SecondaryTextColor:          tcell.ColorDefault,
+    TertiaryTextColor:           tcell.ColorDefault,
+    InverseTextColor:            tcell.ColorDefault,
+    ContrastSecondaryTextColor:  tcell.ColorDefault,
+}
+
 func init() {
     tview.Borders.HorizontalFocus = tview.BoxDrawingsLightHorizontal
     tview.Borders.VerticalFocus = tview.BoxDrawingsLightVertical
@@ -19,6 +33,8 @@ func init() {
     tview.Borders.TopRightFocus = tview.BoxDrawingsLightDownAndLeft
     tview.Borders.BottomLeftFocus = tview.BoxDrawingsLightUpAndRight
     tview.Borders.BottomRightFocus = tview.BoxDrawingsLightUpAndLeft
+    tview.Styles = theme
+
 }
 
 type file struct {
